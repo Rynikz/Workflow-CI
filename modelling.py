efc40ee2dd4e6e4b2faa1fd08ce73948b6bd5a0a
Wahyu <ranggaldrian143@gmail.com>
2025-06-15 23:30:28 +0700

    Fixing workflow error

--- modelling.py
+++ modelling.py
@@ -15,7 +15,7 @@ def main():
 
     mlflow.sklearn.autolog()
 
-    df = pd.read_csv("namadataset_preprocessing/data_bersih.csv")
+    df = pd.read_csv("Kelayakan-pendidikan-indonesia_preprocessing/data_bersih.csv")
     X = df.drop("Status_Kelayakan", axis=1)
     y = df["Status_Kelayakan"]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
